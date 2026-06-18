5f7f59320f228deb4ea040041861f3f67753a063
lukew <lukew@crynux.io>
2026-06-18 09:19:27 +0800

    [bug-fix] node list paging

--- src/api/v2/network.js
+++ src/api/v2/network.js
@@ -2,8 +2,8 @@ import BaseAPI from '../base-api'
 import v2 from './v2'
 
 class V2NetworkAPI extends BaseAPI {
-    getAllNodesData(start, total) {
-        return v2.get('/network/nodes/data?start=' + start + '&total=' + total)
+    getAllNodesData(page, pageSize) {
+        return v2.get('/network/nodes/data?page=' + page + '&page_size=' + pageSize)
     }
 }
 
